4e46960c51f9dc2542130847c4c59156cd0fafc7
tolstovrob <tolstovrob@gmail.com>
2025-07-21 10:38:49 +0400

    feat: wrap rendering with renderApp function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-document.querySelector('#app').innerHTML = `
+import './main.css';
+import { Box } from './components/Box';
+
+function renderApp(root) {
+	root.innerHTML = `
   <div>
     <h1>Hello Vite!</h1>
+    <main></main>
   </div>
 `;
+
+	const TestBox = new Box(document.querySelector('main'));
+	TestBox.render();
+}
+
+renderApp(document.querySelector('#app'));
