35586284488302ed958ed5bebe21c737fc885de3
tolstovrob <tolstovrob@gmail.com>
2025-07-21 12:38:44 +0400

    feat: use shared store for grid state

---
+++ src/store.js
@@ -0,0 +1,50 @@
+export const gridState = {
+	avatar: {
+		row: 0,
+		column: 0,
+		rowSpan: 1,
+		columnSpan: 1,
+	},
+	info: {
+		row: 0,
+		column: 1,
+		rowSpan: 1,
+		columnSpan: 1,
+	},
+	languages: {
+		row: 0,
+		column: 2,
+		rowSpan: 1,
+		columnSpan: 2,
+	},
+	education: {
+		row: 1,
+		column: 0,
+		rowSpan: 3,
+		columnSpan: 2,
+	},
+	interests: {
+		row: 1,
+		column: 2,
+		rowSpan: 2,
+		columnSpan: 2,
+	},
+	contacts: {
+		row: 3,
+		column: 2,
+		rowSpan: 1,
+		columnSpan: 2,
+	},
+	tools: {
+		row: 4,
+		column: 0,
+		rowSpan: 4,
+		columnSpan: 1,
+	},
+	experience: {
+		row: 4,
+		column: 1,
+		rowSpan: 4,
+		columnSpan: 3,
+	},
+};
