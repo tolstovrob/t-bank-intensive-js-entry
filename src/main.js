0bd86cfa25e3dd3489ec933057854e656cbb37ae
tolstovrob <tolstovrob@gmail.com>
2025-07-21 17:58:12 +0400

    revert!: reset to initial project

--- src/main.js
+++ src/main.js
@@ -1,22 +1,11 @@
 import '@/main.css';
-import { Box } from './components/Box';
-
-const gridRows = 12,
-	gridCols = 4,
-	boxes = [
-		{ name: 'avatar', row: 1, col: 1, rows: 1, cols: 1 },
-		{ name: 'info', row: 1, col: 2, rows: 1, cols: 1 },
-	];
 
 function renderApp(root) {
 	root.innerHTML = `
   <div>
-    <main class="grid"></main>
+    <h1>Tilted...</h1>
   </div>
 `;
-
-	const grid = document.querySelector('main.grid');
-	boxes.forEach((data) => grid.appendChild(new Box(data).el));
 }
 
 renderApp(document.querySelector('#app'));
