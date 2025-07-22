fc8dd389681eb98c6b94dce873cc4eb6261f5524
tolstovrob <tolstovrob@gmail.com>
2025-07-22 16:45:15 +0400

    feat: render Grid

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import '@/main.css';
 import { Navbar } from '@/components/Navbar';
+import { Grid } from './components/Grid';
 
 function renderApp(root) {
 	const navbar = new Navbar(root);
 	navbar.render();
+
+	const grid = new Grid(root, []);
+	grid.render();
 }
 
 renderApp(document.querySelector('#app'));
