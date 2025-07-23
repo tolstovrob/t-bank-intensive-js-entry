3bbd694eb777af9b1dbffdfa94952cd5ac47ff30
tolstovrob <tolstovrob@gmail.com>
2025-07-23 19:58:07 +0400

    feat(Avatar): add Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,12 @@
+import { Box } from './Box';
+
+export class Avatar extends Box {
+	constructor(root, options) {
+		super(root, options);
+		this.src = options.src;
+	}
+
+	getInnerComponent() {
+		return `<img src=${this.src} alt="logo" title="logo" class="avatar" />`;
+	}
+}
